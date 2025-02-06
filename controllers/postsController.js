a062f4d2e62a0d860b3c6bb4ea410a2a837d196d
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-06 13:43:07 +0100

    create controller functions

---
+++ controllers/postsController.js
@@ -0,0 +1,53 @@
+// importiamo il modulo contenente l'array di oggetti (posts)
+const posts = require('../data/posts');
+
+// definiamo le logiche per ciascuna rotta
+
+// index
+function index(req, res) {
+    
+    res.json(posts);
+};
+
+
+// show
+function show(req, res) {
+    // Converti id in numero
+    const postId = parseInt(req.params.id);
+    // Trova il post corrispondente
+    const post = posts.find(p => p.id === postId); 
+
+    if (!post) {
+        return res.status(404).json({ error: 'Post non trovato' });
+    }
+
+    res.json(post);
+};
+
+
+// store
+function store(req, res) {
+    res.send('Creazione nuovo post');
+};
+
+
+// update
+function update(req, res) {
+    res.send('Modifica integrale del post ' + req.params.id);
+};
+
+
+// modify
+function modify(req, res) {
+    res.send('Modifica parziale del post ' + req.params.id);
+};
+
+
+// destroy
+function destroy(req, res) {
+    res.send('Eliminazione del post ' + req.params.id);
+};
+
+
+// esportiamo il modulo del router
+module.exports = {index, show, store, update, modify, destroy}
--- routers/postsRouters.js
+++ routers/postsRouters.js
@@ -1,53 +1,37 @@
 // importiamo express e utlizziamo la parte di routing
 const express = require('express')
 const router = express.Router();
-const posts = require('../data/posts');
+// const posts = require('../data/posts');
 
 
 // rotte di CRUD dei posts
 // index
 router.get('/', function (req, res) {
-    
-    res.json(posts);
 });
 
 
 // show
 router.get('/:id', function (req, res) {
-    // Converti id in numero
-    const postId = parseInt(req.params.id);
-    // Trova il post corrispondente
-    const post = posts.find(p => p.id === postId); 
-
-    if (!post) {
-        return res.status(404).json({ error: 'Post non trovato' });
-    }
-
-    res.json(post);
 });
 
 
 // store
 router.post('/', function (req, res) {
-    res.send('Creazione nuovo post');
 });
 
 
 // update
 router.put('/:id', function (req, res) {
-    res.send('Modifica integrale del post ' + req.params.id);
 });
 
 
 // modify
 router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale del post ' + req.params.id);
 });
 
 
 // destroy
 router.delete('/:id', function (req, res) {
-    res.send('Eliminazione del post ' + req.params.id);
 });
 
 
