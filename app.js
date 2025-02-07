b7f4614d7afb45cd687260b93683496f5bf17119
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-07 17:23:16 +0100

    fix app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+// importa il modulo express
 const express = require('express')
+// crea l'istanza dell'applicazione express
 const app = express()
+// definisci la porta da utilizzare
 const port = 3000
 
 // importiamo il router per l'entita post
@@ -11,12 +14,11 @@ app.use(express.json());
 // definiamo l'uso di una cartella per i file statici
 app.use(express.static('public'));
 
-
 // utilizziamo la rotta dei posts andando a definire la parte iniziale delle rotte
 app.use("/posts", postsRouter)
 
 
-
+// avviai l server e mettilo in ascolto sulla porta selezionata
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
